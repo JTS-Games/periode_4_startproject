6783eb3915ee041a0da0b76045f83f8e66f2aa89
JTS-G <JTS-Games@email.com>
2024-05-07 15:19:40 +0200

    second fish

--- src/js/game.js
+++ src/js/game.js
@@ -16,6 +16,12 @@ export class Game extends Engine {
         fish.pos = new Vector(400, 300)
         fish.vel = new Vector(-100,0)
         this.add(fish)
+
+        const fish2 = new Actor()
+        fish2.graphics.use(Resources.Fish.toSprite())
+        fish2.pos = new Vector(400, 300)
+        fish2.vel = new Vector(100,0)
+        this.add(fish2)
     }
 }
 
